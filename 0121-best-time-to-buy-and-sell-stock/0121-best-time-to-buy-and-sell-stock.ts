daf8d6651f9b26bbdc6b5ccbda4f3e44bcacb76c
G-hoon Bae <new0126@naver.com>
2025-01-06 16:31:33 +0900

    Time: 5 ms (25.81%), Space: 60.8 MB (17.86%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
@@ -0,0 +1,15 @@
+function maxProfit(prices: number[]): number {
+    let minPrice = Infinity;  
+    let maxProfit = 0;      
+    
+    for (let price of prices) {
+        if (price < minPrice) {
+            minPrice = price;
+        }
+        else if (price - minPrice > maxProfit) {
+            maxProfit = price - minPrice;
+        }
+    }
+    
+    return maxProfit;
+};
