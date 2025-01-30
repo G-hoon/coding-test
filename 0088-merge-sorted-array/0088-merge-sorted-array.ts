6bb5599bcf91dd57f1ae77a13d4ca4b6720d0b26
G-hoon Bae <new0126@naver.com>
2025-01-31 01:14:13 +0900

    Time: 0 ms (100%), Space: 51.1 MB (96.62%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.ts
@@ -0,0 +1,10 @@
+/**
+ Do not return anything, modify nums1 in-place instead.
+ */
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+
+    for (let index = m; index < nums1.length; ++index) {
+        nums1[index] = nums2[index - m]
+    }
+    nums1.sort((a, b) => a - b)
+};
