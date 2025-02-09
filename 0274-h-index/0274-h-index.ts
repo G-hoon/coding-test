9dec0b1e5fd3e2a417b5c735c132a1897e361f4e
G-hoon Bae <new0126@naver.com>
2025-02-10 01:07:57 +0900

    Time: 1 ms (80.5%), Space: 51.9 MB (38.42%) - LeetHub

--- 0274-h-index/0274-h-index.ts
+++ 0274-h-index/0274-h-index.ts
@@ -3,7 +3,7 @@ function hIndex(citations: number[]): number {
     const sortedCitations = citations.sort((a,b) => b-a)
 
     for (let index = 0; index < citations.length; ++index) {
-        console.log(sortedCitations[index], ' / ', index + 1)
+
         if (sortedCitations[index] < index + 1) {
             return index 
         }
